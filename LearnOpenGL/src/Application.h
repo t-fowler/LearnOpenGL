d50360cce88725b12858adfdbbf442a99d2965f5
Tyler Fowler <tfowl@proton.me>
2022-08-06 00:34:44 -0700

    Added credit for error checking code.

--- LearnOpenGL/src/Application.h
+++ LearnOpenGL/src/Application.h
@@ -2,6 +2,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// Credit to youtube.com/TheChernoProject for the error checking code.
 #define ASSERT(x) if (!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
 	x;\
